16484b3cff5eb7391b67e0a9142607e8b84491b5
Pari <ppandya469@berkeley.edu>
2024-05-21 16:35:07 -0700

    Added clear and delete buttons to frame.

--- src/Calculator.java
+++ src/Calculator.java
@@ -26,6 +26,46 @@ public class Calculator implements ActionListener{
         textfield.setFont(myFont); //referencing the created font
         textfield.setEditable(false); //limits user input to certain characters
 
+        //Buttons
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Delete");
+        clrButton = new JButton("Clear");
+
+        //Adding buttons to array
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+
+        //iterating through the functionButtons
+        for(int i = 0; i < 8; i++){
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false); //removes button outline
+        }
+
+        //iterating through the numberButtons
+        for(int i = 0; i < 10; i++){
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false); //removes button outline
+        }
+
+        delButton.setBounds(50,430,140,50);
+        clrButton.setBounds(205,430,140,50);
+
+        frame.add(delButton);
+        frame.add(clrButton);
         frame.add(textfield); //adding textfield to the frame
         frame.setVisible(true); //visible
     }
